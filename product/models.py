5f559000778e5133ea4bd995ec12003e0bbcf948
Amsiam <amsiam990@gmail.com>
2021-08-01 17:27:17 +0600

    readme update

--- product/models.py
+++ product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    added_by = models.ForeignKey(User,on_delete=models.CASCADE)
