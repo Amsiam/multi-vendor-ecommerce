5b0c7ec579fdc5e1c57845fddf1e35ff57b3a998
Amsiam <amsiam990@gmail.com>
2021-08-01 19:41:25 +0600

    product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
-
+from .models import ExtraImages, Product
+from product.models import DiscountType
+from datetime import datetime
 # Register your models here.
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ExtraImages
+    max_num = 5
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+    list_display = ["title", "price", "discount_price", "category",
+                    "in_stock",  "status"]
+
+    fieldsets = (
+        (None, {
+            "fields": ("title", "price", "category", "thumbnail", "description", "in_stock", "status"),
+        }),
+        ("Discount", {
+            "fields": ("discount_type", "discount", "discount_start", "discount_end"),
+        })
+    )
+
+    def save_model(self, request, obj, form, change):
+
+        if(not change):
+            obj.added_by = request.user
+        return super().save_model(request, obj, form, change)
+
+    def discount_price(self, obj):
+        if(obj.discount_type == DiscountType.none):
+            return "No Discount Running on"
+        else:
+            timenow = datetime.now()
+            if(obj.discount_start <= timenow and timenow <= obj.discount_end):
+                if(obj.discount_type == DiscountType.money):
+                    return obj.price - obj.discount
+                else:
+                    return obj.price - (obj.discount*0.01*obj.price)
+            else:
+                return "No Discount Running on"
+
+    readonly_fields = ('slug',)
+
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,9 +1,74 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from category.models import Category
 
 User = get_user_model()
 
+
+class DiscountType(models.TextChoices):
+    money = "M", "Money"
+    percent = "P", "Percent"
+    none = "N", "None"
+
+
+class ProductStatus(models.TextChoices):
+    PUBLISHED = "PB", "Published"
+    DRAFT = "DR", "Draft"
+
+
 class Product(models.Model):
+    # about product
     title = models.CharField(max_length=255)
+    price = models.FloatField(default=0)
     description = models.TextField(blank=True)
-    added_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, null=True, on_delete=models.SET_NULL, related_name="products")
+    status = models.CharField(
+        max_length=2, default=ProductStatus.DRAFT, choices=ProductStatus.choices)
+    in_stock = models.IntegerField()
+
+    thumbnail = models.ImageField(upload_to="product/thumbnail")
+    slug = models.SlugField(unique=True)
+
+    # about seller
+    added_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="products")
+
+    # about discount
+    discount_type = models.CharField(
+        max_length=1, default=DiscountType.none, choices=DiscountType.choices)
+    discount = models.FloatField(blank=True, null=True)
+    discount_start = models.DateTimeField(blank=True, null=True)
+    discount_end = models.DateTimeField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+
+        if(not self.slug):
+            self.slug = slugify(self.title,allow_unicode=True)
+
+            while(Product.objects.filter(slug=self.slug).count()):
+                self.slug += "-"+str(len(self.slug))
+
+        return super(Product, self).save(*args, **kwargs)
+
+
+class ExtraImages(models.Model):
+
+    product = models.ForeignKey(
+        "product", on_delete=models.CASCADE, related_name="extraimages")
+    images = models.ImageField(upload_to="product/thumbnail")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Extra Image"
+        verbose_name_plural = "Extra Images"
